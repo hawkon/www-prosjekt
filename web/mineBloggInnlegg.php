ca5344ee756157a3b2c588fe2a7a09623d8959d7
hawkon <graslauk@gmail.com>
2013-05-09 23:50:20 +0200

    jees

---
+++ web/mineBloggInnlegg.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+
+
+
+if (!isset($_SESSION['user']))
+
+	die ('Du er jo ikke logget på!!!!');
+
+?>
+
+<a style="float: right" href="javascript:newBlogEntry();">Nytt blog innlegg</a>
+
+<ul class="myBlogEntries">
+
+<?php
+
+require_once 'db.php';
+
+$sql = 'SELECT id, title, entry, lat, lng, DATE_FORMAT(`when`, "%a %e/%c-%Y (%k:%i)") as `date` FROM entry WHERE uid=? ORDER BY `when` DESC limit 20';
+
+$sth = $db->prepare ($sql);
+
+$sth->execute (array ($_SESSION['user']));
+
+while ($row = $sth->fetch()) {
+
+	echo "<li><a href='javascript:showEntry({$row['id']}, \"mineBloggInnlegg.php\")'>{$row['title']}</a> ({$row['date']})</li>";
+
+}
+
+?>
+
+</ul>
--- web/pub.js
+++ web/pub.js
@@ -128,3 +128,65 @@ $(document).ready (function () {
     }
   });
 });
+
+
+
+
+
+
+
+function newBlogEntry () {
+
+	$.ajax({
+
+		url: 'newBlogEntry.html',
+
+		success: function (data) {
+
+			$('#content').html (data);
+
+			$('#newBlogEntry').tinymce({
+
+				language : 'en', 
+
+				// Location of TinyMCE script
+
+				script_url : '/tinymce/jscripts/tiny_mce/tiny_mce_gzip.php',
+	
+
+
+				// General options
+
+				theme : "advanced",
+
+				plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,advlist,spellchecker",
+
+
+
+				// Theme options
+
+				theme_advanced_buttons1 : "spellchecker,iespell,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontselect,fontsizeselect,|,forecolor,backcolor",
+
+				theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,removeformat,help,code,|,visualchars,nonbreaking,visualaid,|,insertdate,inserttime",
+
+				theme_advanced_buttons3 : "tablecontrols,|,sub,sup,|,charmap,emotions,media,advhr,|,print,|,fullscreen,|,cite,abbr,acronym,del,ins",
+
+				theme_advanced_toolbar_location : "top",
+
+				theme_advanced_toolbar_align : "left",
+
+				theme_advanced_statusbar_location : "bottom",
+
+				theme_advanced_resizing : true,
+
+			
+
+				spellchecker_languages : "Norwegian=no,+English=en",
+
+			});
+
+		}
+
+	});
+
+}
