5bb3e75bb6f579b75d1e847555a01b869f4d6596
hawkon <graslauk@gmail.com>
2013-05-10 00:29:17 +0200

    hesdge33

--- web/pub.js
+++ web/pub.js
@@ -190,50 +190,32 @@ function newBlogEntry () {
 
 }
 
-<?php
+function storeNewBlogEntry() {
 
-	session_start();
+	$.ajax({
 
-	require_once 'db.php';
+		url: 'storeNewBlogEntry.php',
 
-	
+		data: {title: $('#content form input')[0].value, content: $('#newBlogEntry').html(), latitude: latitude, longitude: longitude },
 
-	if (!isset($_SESSION['user']))
+		type: 'post',
 
-		die (json_encode (array ('message'=>'Du kan ikke lagre blog innlegg når du ikke er logget på')));
+		success: function (tmp) {
 
-	$sql = 'INSERT INTO entry (uid, title, entry, lat, lng, `when`) VALUES (?, ?, ?, ?, ?, now())';
+			data = eval ('('+tmp+')');
 
-	if (isset($_POST['latitude']))
+			if (data.ok) {
 
-		$lat = $_POST['latitude'];
+				alert (data.message);
 
-	else
+				$('#content').load ('mineBloggInnlegg.php');
 
-		$lat = null;
+			} else
 
-	if (isset($_POST['longitude']))
+				alert (data.message);
 
-		$lng = $_POST['longitude'];
+		}
 
-	else
+	});
 
-		$lng = null;
-
-	$sth = $db->prepare ($sql);
-
-//	print_r ($db->errorInfo());
-
-	$sth->execute (array ($_SESSION['user'], $_POST['title'], $_POST['content'], $lat, $lng));
-
-//	print_r ($db->errorInfo());
-
-	if ($sth->rowCount()==1)
-
-		die (json_encode (array('ok'=>'OK', 'message'=>'Nytt blogg innlegg lagret i databasen')));
-
-	else
-
-		die (json_encode (array('message'=>'Problemer oppsto ved lagring av innlegget i databasen')));
-
-?>
+}
