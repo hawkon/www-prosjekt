292544ec549f45140471f528b16fff45e17593f5
hawkon <graslauk@gmail.com>
2013-05-10 02:07:25 +0200

    teste

--- web/pub.js
+++ web/pub.js
@@ -219,3 +219,35 @@ function storeNewBlogEntry() {
 	});
 
 }
+
+function addElement() {
+
+  var ni = document.getElementById('myDiv');
+
+  var numi = document.getElementById('theValue');
+
+  var num = (document.getElementById('theValue').value -1)+ 2;
+
+  numi.value = num;
+
+  var newdiv = document.createElement('div');
+
+  var divIdName = 'my'+num+'Div';
+
+  newdiv.setAttribute('id',divIdName);
+
+  newdiv.innerHTML = 'Element Number '+num+' has been added! <a href=\'#\' onclick=\'removeElement('+divIdName+')\'>Remove the div "'+divIdName+'"</a>';
+
+  ni.appendChild(newdiv);
+
+}
+
+function removeElement(divNum) {
+
+  var d = document.getElementById('myDiv');
+
+  var olddiv = document.getElementById(divNum);
+
+  d.removeChild(olddiv);
+
+}
